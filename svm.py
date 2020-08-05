de0c897fb665c05c32c485480f823e2319a56756
Kailash Turimella <kailash.turimella@gmail.com>
2020-08-05 09:02:41 -0700

    Add files via upload

---
+++ svm.py
@@ -0,0 +1,86 @@
+"""
+Support Vector Machine (SVM)
+
+It finds the two closest points of different categories.
+Those two points are called the support vectors.
+All the other points are completely ignored and do not influence the model in any way.
+It then finds a line equidistant from both the points and with the maximum margin.
+Margin is the distance between any one of the points and the line.
+
+SVM is different from most other methods because it predicts based on the extremes
+For example, if your trying to classify apples and oranges,
+   most of the algorithms learn from the apples which look a lot like apples
+   but instead, svm learns based on the apple that looks like an orange, or the worst apple.
+
+"""
+# Importing libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+
+# Importing datasets
+dataset = pd.read_csv('Social_Network_Ads.csv')
+X = dataset.iloc[:,[2,3]].values     # Independent         # All rows of all columns except the last one
+y = dataset.iloc[:,4].values         # Dependent           # All rows of the third column
+
+# Splitting the data set into training set and test set
+from sklearn.model_selection import train_test_split 
+X_train,X_test,y_train,y_test = train_test_split(X,y, test_size = 0.25,random_state = 0) # test size = 20% 
+
+
+# Feature scaling
+#        bringing the age and salary to a specific range
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+sc_y = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)          # Already fitted
+
+# Fitting classifier to training set
+from sklearn.svm import SVC
+classifier = SVC(kernel = 'linear')   # Seperated by a straight line
+classifier.fit(X_train,y_train)
+
+# Predicting test set results
+y_pred = classifier.predict(X_test)
+
+# Making the confusion matrix
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test,y_pred)
+
+# Visualising the Training set results
+from matplotlib.colors import ListedColormap
+X_set, y_set = X_train, y_train
+X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
+                     np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
+plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+             alpha = 0.75, cmap = ListedColormap(('red', 'green')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
+                c = ListedColormap(('red', 'green'))(i), label = j)
+plt.title('SVM (Training set)')
+plt.xlabel('Age')
+plt.ylabel('Estimated Salary')
+plt.legend()
+plt.show()
+
+# Visualising the Test set results
+from matplotlib.colors import ListedColormap
+X_set, y_set = X_test, y_test
+X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
+                     np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
+plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+             alpha = 0.75, cmap = ListedColormap(('red', 'green')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
+                c = ListedColormap(('red', 'green'))(i), label = j)
+plt.title('SVM (Test set)')
+plt.xlabel('Age')
+plt.ylabel('Estimated Salary')
+plt.legend()
+plt.show()
