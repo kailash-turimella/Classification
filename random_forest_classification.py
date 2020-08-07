8ab837d0e9a827a5066ca29a598aec6231efa7e1
Kailash Turimella <kailash.turimella@gmail.com>
2020-08-07 13:44:19 -0700

    Add files via upload

---
+++ random_forest_classification.py
@@ -0,0 +1,84 @@
+"""
+Random Forest
+
+Ensemble Learning - When you take multiple machine learning algorithms and combine them.
+Random Forest combines multiple decision trees
+
+Pick random points from your dataset
+Build a decision tree of those points(instead of building a decision tree of all the points)
+Choose the number of trees you want(n_estimators) and repeat steps 1&2.
+You will have multiple decision trees
+For a new point, which ever category most of the decision trees predict(majority vote) is assigned
+
+"""
+# Importing libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+
+# Importing datasets
+dataset = pd.read_csv('Social_Network_Ads.csv')
+X = dataset.iloc[:,[2,3]].values     # Independent         # All rows of all columns except the last one
+y = dataset.iloc[:,4].values         # Dependent           # All rows of the third column
+
+# Splitting the data set into training set and test set
+from sklearn.model_selection import train_test_split 
+X_train,X_test,y_train,y_test = train_test_split(X,y, test_size = 0.25,random_state = 0) # test size = 20% 
+
+
+# Feature scaling
+#        bringing the age and salary to a specific range
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+sc_y = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)          # Already fitted
+
+# Fitting classifier to training set
+from sklearn.ensemble import RandomForestClassifier
+classifier = RandomForestClassifier(n_estimators = 100000, criterion = 'entropy')
+classifier.fit(X_train,y_train)
+
+# Predicting test set results
+y_pred = classifier.predict(X_test)
+
+# Making the confusion matrix
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test,y_pred)
+
+# Visualising the Training set results
+from matplotlib.colors import ListedColormap
+X_set, y_set = X_train, y_train
+X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
+                     np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
+plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+             alpha = 0.75, cmap = ListedColormap(('red', 'green')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
+                c = ListedColormap(('red', 'green'))(i), label = j)
+plt.title('Random Forest Classification (Training set)')
+plt.xlabel('Age')
+plt.ylabel('Estimated Salary')
+plt.legend()
+plt.show()
+
+# Visualising the Test set results
+from matplotlib.colors import ListedColormap
+X_set, y_set = X_test, y_test
+X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
+                     np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
+plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+             alpha = 0.75, cmap = ListedColormap(('red', 'green')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
+                c = ListedColormap(('red', 'green'))(i), label = j)
+plt.title('Random Forest Classification (Test set)')
+plt.xlabel('Age')
+plt.ylabel('Estimated Salary')
+plt.legend()
+plt.show()
