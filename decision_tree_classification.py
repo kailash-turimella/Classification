b7fee49a163292eebf190becb7c0f2cde03d23e8
Kailash Turimella <kailash.turimella@gmail.com>
2020-08-09 16:51:44 -0700

    Update decision_tree_classification.py

--- decision_tree_classification.py
+++ decision_tree_classification.py
@@ -21,7 +21,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-
 # Importing datasets
 dataset = pd.read_csv('Social_Network_Ads.csv')
 X = dataset.iloc[:,[2,3]].values     # Independent         # All rows of all columns except the last one
